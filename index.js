180d8f82fe3d1078d797b520d15f93610a80b821
Jisun696 <istiakkashem35@gmail.com>
2024-04-30 20:10:14 +0600

    product delete api done

--- index.js
+++ index.js
@@ -57,6 +57,12 @@ async function run() {
       res.send(result);
     });
 
+    app.delete('/deleteProduct/:id', async (req, res) => {
+      const query = { _id: new ObjectId(req.params.id) };
+      const result = await productCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
